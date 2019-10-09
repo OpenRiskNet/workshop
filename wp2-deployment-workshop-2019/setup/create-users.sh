f5345b1c2df704cb2eb996169b4264cfbb25dd38
Alan Christie <alan.christie@matildapeak.com>
2019-10-09 15:55:06 +0100

    - Minor tweaks

--- wp2-deployment-workshop-2019/setup/create-users.sh
+++ wp2-deployment-workshop-2019/setup/create-users.sh
@@ -18,10 +18,10 @@ touch htpasswd
 for i in $(seq 1 "$1")
 do
   username=user$i
-  password=$(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c"${1:-12}";echo;)
+  password=$(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c12;echo;)
   echo "${username},${password}" >> users.txt
   sudo useradd -m -s /bin/bash "$username"
-  echo "$password" | passwd "$username" --stdin > /dev/null
+  echo "$password" | sudo passwd "$username" --stdin > /dev/null
   sudo htpasswd -b htpasswd "$username" "$password" &> /dev/null
   echo "Created user $username"
 done
