8476765ff072121330c875d401ea9d2048a20e6d
Alan Christie <alan.christie@matildapeak.com>
2019-10-10 12:56:00 +0100

    - Minor tweak

--- wp2-deployment-workshop-2019/setup/create-users.sh
+++ wp2-deployment-workshop-2019/setup/create-users.sh
@@ -55,6 +55,7 @@ do
 done
 
 # Get OC (into /bin)
+echo "Getting oc..."
 wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > /dev/null 2>&1
 tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > /dev/null 2>&1
 sudo cp openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /bin
